dde6a30d38fbc5ef69afa86209c116db81b512c6
wuliJerry <wuliJerry927@outlook.com>
2023-04-06 23:03:26 +0800

    read args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![allow(unused)]
+
+mod utils;
+use utils::args::*;
+
 fn main() {
-    println!("Hello, world!");
+    let path: Option<String> = match parse_args() {
+        Ok(p) => {
+            Some(p)
+        }
+        Err(CommandLineError::MissingArgument) => {
+            eprintln!("Missing argument");
+            std::process::exit(1);
+        }
+        Err(CommandLineError::TooManyArguments) => {
+            eprintln!("Too many arguments");
+            std::process::exit(1);
+        }
+    };
 }
